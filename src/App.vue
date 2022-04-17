c840ca35eddba26514d2ee991c1e95be864d00e6
Daniel B <daniel.984@hotmail.com>
2022-04-17 17:45:20 -0500

    Application Working

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Indecition />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Indecition from "./components/Indecition.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Indecition,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Indecition.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <img v-if="img" class="bg" :src="img" alt="bg">
+    <div class="bg-dark"></div>
+
+    <div class="indecition-container">
+      <input
+        v-model="question"
+        type="text"
+        placeholder="Hazme un pregunta :D"
+      />
+      <p>Recuerda terminar con un signo de interrogación (?)</p>
+
+      <div>
+        <h2>{{ question }}</h2>
+        <h1>{{ answer }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      question: null,
+      answer: null,
+      img: null,
+    };
+  },
+  methods: {
+    async getAnswer() {
+      this.answer = ".... Pensando";
+
+      const { answer, image } = await fetch("https://yesno.wtf/api").then((r) =>
+        r.json()
+      );
+
+      this.answer = answer.toUpperCase();
+      this.img = image;
+    },
+  },
+  watch: {
+    question(value, oldValue) {
+      if (!value.includes("?")) return;
+
+      this.getAnswer();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.bg {
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-attachment: fixed;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+.bg-dark {
+  background-color: rgba(0, 0, 0, 0.4);
+  height: 100vh;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+  width: 100vw;
+}
+.indecition-container p {
+  margin-top: 5px;
+  font-size: 16px;
+  color: rgb(206, 206, 206);
+}
+
+.indecition-container {
+  position: relative;
+  z-index: 99;
+}
+
+input {
+  width: 350px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: none;
+}
+input:focus {
+  outline: none;
+}
+
+p {
+  color: white;
+  font-size: 20px;
+  margin-top: 0px;
+}
+
+h1,
+h2 {
+  color: white;
+}
+
+h2 {
+  margin-top: 150px;
+}
+</style>
